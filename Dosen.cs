4ca6d17858f3a51b137707b1b0cfd706ed7e8699
Feysha Rahadian Syah <101683544+alstroemeriaP@users.noreply.github.com>
2022-05-18 09:44:54 +0700

    -

---
+++ Dosen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts_pemrograman
+{
+    public class Dosen
+    {
+        public Dosen()
+        {
+
+        }
+        public Dosen(int Id, string gender)
+        {
+            this.Id = Id;
+            Gender = gender;
+        }
+
+        public int Id { get; set; }
+        public string? Gender { get; set; }
+
+    }
+    public class AttDosen
+    {
+        public AttDosen()
+        {
+
+        }
+        public AttDosen(string nama, int nik)
+        {
+            Nama = nama;
+            Nik = nik;
+        }
+        public int Nik { get; set; }
+        public string? Nama { get; set; }
+    }
+
+    public class AddCourse
+    {
+        public AddCourse()
+        {
+
+        }
+        public AddCourse(string course)
+        {
+            Course = course;
+        }
+        public string? Course { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts_pemrograman
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "UTS PEMROGRAMAN";
+
+            Dosen dosen1 = new Dosen();
+            AttDosen dosen2 = new AttDosen();
+            AddCourse dosen3 = new AddCourse();
+
+            //input data
+            Console.WriteLine("=== Input Data Dosen ===\n");
+
+            Console.Write("ID     : ");
+            dosen1.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nama   : ");
+            dosen2.Nama = Console.ReadLine();
+
+            Console.Write("NIK    : ");
+            dosen2.Nik = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Gender : ");
+            dosen1.Gender = Console.ReadLine();
+
+            Console.Write("Course : ");
+            dosen3.Course = Console.ReadLine();
+
+
+            //output data
+            Console.WriteLine("\n=== Output ===\n");
+            Console.WriteLine("ID     : {0}", dosen1.Id);
+            Console.WriteLine("Nama   : {0}", dosen2.Nama);
+            Console.WriteLine("NIK    : {0}", dosen2.Nik);
+            Console.WriteLine("Gender : {0}", dosen1.Gender);
+            Console.WriteLine("Course : {0}", dosen3.Course);
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
